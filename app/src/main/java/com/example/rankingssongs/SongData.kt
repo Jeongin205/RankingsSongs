5b07a1e656ce5941544ae1d5decdaf797f9bf3e6
Jeongin205 <81507170+Jeongin205@users.noreply.github.com>
2021-09-19 02:32:33 +0900

    a

--- app/src/main/java/com/example/rankingssongs/SongData.kt
+++ app/src/main/java/com/example/rankingssongs/SongData.kt
@@ -1,10 +1,11 @@
 package com.example.rankingssongs
 
+import com.example.rankingssongs.data.Track
+import com.example.rankingssongs.data.Tracks
 import com.google.gson.annotations.SerializedName
 
 data class SongData(
     val number:String,
-    @SerializedName("{"tracks":{"track")
     val song: String,
     val singer: String
 )
